177c2f267adaffd279b8765f3665dfe393ec80f3
fanbrightup <fanbright@126.com>
2016-10-15 11:38:41 +0800

    刷新系统

---
+++ week41/js特效实例/2第二章/demo1_问候语.js
@@ -0,0 +1,6 @@
+var date = new Date();
+var month = date.getMonth()+1;
+var day = date.getDate();
+if(month == 10 || day == 14){
+  console.log('10-14');
+}
---
+++ week41/js特效实例/2第二章/test.js
@@ -0,0 +1,11 @@
+var m = 3;
+switch (m) {
+  case 1:console.log('1');
+    break;
+    case 2:console.log('2');
+      break;
+      case 3:console.log('3');
+        break;
+  default:
+
+}
